e3a4cb5adfd874c1bf5eda3206ace0b4022c1276
wonseoke <wonseoke@gmail.com>
2020-06-10 22:36:09 -0400

    Update reporter.py

--- reporter.py
+++ reporter.py
@@ -6,7 +6,7 @@ import datetime
 
 now = datetime.datetime.now()
 
-print("SALES REPORT" , f"MONTH: {month} {year}")
+print("SALES REPORT", f"MONTH: {month} {year}")
 
 sum = 0
 
@@ -17,22 +17,21 @@ with open(csv_filepath, "r") as csv_file:
     for row in reader:
         sum += float(row["sales price"])
 
+print(sum)
 
+##def to_usd(my_price):
 
+  #  Converts a numeric value to usd-formatted string, for printing and display purposes.
 
-def to_usd(my_price):
-    """
-    Converts a numeric value to usd-formatted string, for printing and display purposes.
-
-    Param: my_price (int or float) like 4000.444444
+  #  Param: my_price (int or float) like 4000.444444
     
-    Example: to_usd(4000.444444)
+  #  Example: to_usd(4000.444444)
     
-    Returns: $4,000.44
-    """
-    return f"${my_price:,.2f}" #> $12,000.71
+  #  Returns: $4,000.44
+    
+  ##  return f"${my_price:,.2f}" #> $12,000.71
 
-print("TOTAL SALES:", to_usd(sum))
+##print("TOTAL SALES:", to_usd(sum))
 
 
 """ print([p for p in reader if p["product"]] == "Sticker Pack") """
